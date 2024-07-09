fda96227ae09c0f0e42eef7395432da2218e8cc3
Alejandro <alejandromerlina324@gmail.com>
2024-07-09 08:47:54 -0700

    se termino el jeugo

--- game.gd
+++ game.gd
@@ -5,13 +5,16 @@ var enemigoEscena=preload("res://enemigo.tscn")
 
 var enemigos_Matar: int
 @onready var label_enemigos=$CanvasLayer/HBoxContainer/LabelEnemigos
+
 func _ready():
-	enemigos_Matar= randf_range(10,90)
+	enemigos_Matar= randf_range(1,3)
 	label_enemigos.text= str(enemigos_Matar)
 	$Timer.connect("timeout",CrearEnemigo)
 	print(" voy a crear un enemigo")
+	$VictoriaCanvasLayer2.visible=false
 	
 func CrearEnemigo():
+	
 	print("enemigo creado")
 	var enemigocreado= enemigoEscena.instantiate()
 	enemigocreado.global_position =Vector2(randf_range(0,1157),randf_range(0,642))
@@ -23,3 +26,17 @@ func bajarcontador():
 	label_enemigos.text= str(enemigos_Matar)
 	if enemigos_Matar <=0:
 		get_tree().paused = true
+		$VictoriaCanvasLayer2.visible=true
+		
+func _on_button_reiniciar_pressed():
+	#enemigos_Matar= randf_range(1,3)
+	#label_enemigos.text= str(enemigos_Matar)
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+	$VictoriaCanvasLayer2.visible=false
+	
+
+
+func _on_button_salir_pressed():
+	get_tree().quit()
+	
